74a24a8fbd738633d90d9762e2bb18c5005d912b
Zacharie Souga <ingenieurias@gmail.com>
2025-10-03 21:56:13 +0200

    Lecture du dataset train

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
-import streamlit as st 
+import streamlit as st
+import pandas as pd 
 
 #Titre de notre application 
 st.title("Application de ML .🤖")
 
 st.info("Cette application est un modèle de machine learning")
+
+train = pd.read_csv("data/train.csv")
+train
