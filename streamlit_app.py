e3ca83ef8fbe43fe3aecd1ba30a6aee9f2964f12
Zacharie Souga <ingenieurias@gmail.com>
2025-10-04 06:15:24 +0200

    Ajout du scatter_plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,18 @@ st.title("Application de ML .🤖")
 
 st.info("Cette application est un modèle de machine learning")
 
-train = pd.read_csv("data/train.csv")
-train
+with st.expander("Données"):
+    st.write('**La donnée brute**')
+    train = pd.read_csv("data/train.csv", index_col="PassengerId")
+    train
+    
+    st.write("**X**")
+    X = train.drop(labels=["Survived"], axis=1)
+    X
+    
+    st.write("**y**")
+    y = train.Survived
+    y
+    
+    with st.expander("Visualisation de données"):
+        st.scatter_chart(data=train, x="Age", y="Fare", color="Survived")
