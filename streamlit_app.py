503c0ebcc50124edf2c75b94d47e0f1cc900a5d1
Zacharie Souga <ingenieurias@gmail.com>
2025-10-04 07:14:46 +0200

    Sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,19 @@ with st.expander("Données"):
     
 with st.expander("Visualisation de données"):
     st.scatter_chart(data=train, x="Age", y="Fare", color="Survived")
+    
+#Préparation de données
+with st.sidebar:
+    st.header("Nos features")
+    "Pclass","Age","SibSp","Parch","Fare",
+    sex = st.selectbox("Sex", ("male", "female"))
+    embarked = st.selectbox("Embarked", ("S", "C", "Q"))
+    pclass = st.slider("Selectionnez votre class", min_value=1, max_value=3, value=1, step=1)
+    st.write(f"Pclass : {pclass}")
+    age = st.slider("Selectionnez votre âge", min_value=0, max_value=100, value=0, step=0.1)
+    st.write(f"Age : {age}")
+    
+    #","SibSp","Parch","Fare"
+    
+    
+    
