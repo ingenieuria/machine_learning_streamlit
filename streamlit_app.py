046697c80ba9d53afff73758a70549486c99ed16
Zacharie Souga <ingenieurias@gmail.com>
2025-10-04 07:52:21 +0200

    Sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ with st.expander("Données"):
     train
     
     st.write("**X**")
-    X = train.drop(labels=["Survived"], axis=1)
+    X = train.drop(labels=["Survived", "Ticket"], axis=1)
     X
     
     st.write("**y**")
@@ -25,12 +25,51 @@ with st.expander("Visualisation de données"):
 #Préparation de données
 with st.sidebar:
     st.header("Nos features")
-    sex = st.selectbox("Sex", ("male", "female"))
-    embarked = st.selectbox("Embarked", ("S", "C", "Q"))
+    
     pclass = st.slider("Selectionnez votre class", min_value=1, max_value=3, value=1, step=1)
     st.write(f"Pclass : {pclass}")
+    
+    name = st.selectbox("Title",("Mr", "Miss", "Rev", "Llord"))
+    st.write(f"Titles : {name}")  
+    
+    sex = st.selectbox("Sex", ("male", "female"))
+    
+
     age = st.slider("Selectionnez votre âge", min_value=0.0, max_value=100.0, value=0.0, step=0.1)
     st.write(f"Age : {age}")
+    
+    sibsp = st.slider("Selectionnez votre class", min_value=1, max_value=10, value=0, step=1)
+    st.write(f"SibSp : {sibsp}")
+    
+    parch = st.slider("Selectionnez votre class", min_value=1, max_value=10, value=0, step=1)
+    st.write(f"Parch : {parch}")
+    
+    fare = st.slider("Selectionnez votre âge", min_value=0.0, max_value=100.0, value=0.0, step=0.1)
+    st.write(f"Fare : {fare}")
+    
+    embarked = st.selectbox("Embarked", ("S", "C", "Q"))
+    
+    
+#Creation d'un DataFrame pour nos features
+data_df = {    
+    "pclass":pclass,
+    "name" : name,
+    "sex" :sex,
+    "age" :age,
+    "sibsp" :sibsp,
+    "parch" :parch,
+    "fare" :fare,
+    "embarked" :embarked
+        }
+
+input_df = pd.DataFrame(data_df, index=[0])
+input_titanic = pd.concat([input_df, X], axis=0)
+
+with st.expander("Les inout features"):
+    st.write("**Input penguin**")
+    input_df
+    st.write("**Combine titanic data**")
+    input_titanic
     
    
     
